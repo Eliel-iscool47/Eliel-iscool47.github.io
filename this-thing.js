e74adf15373c0fe69e93ed1cf90605217a1de9cc
Eliel-iscool47 <elieliscool47@yahoo.com>
2025-12-21 20:02:02 -0500

    Add files via upload

--- this-thing.js
+++ this-thing.js
@@ -1,17 +1,42 @@
-const pi = Math.pi
-const main = document.getElementById('main');
-const ctx = canvas.getContext('2d');
-ctx.fillStyle = 'red';
-ctx.strokeStyle='red';
-ctx.fillRect(0,0,40,40);
-ctx.fillStyle='blue';
-ctx.fillRect(1326,728,40,40);
-ctx.beginPath();
-ctx.moveTo(40,40);
-ctx.lineTo(1326,728);
-ctx.lineWidth=3;
-ctx.stroke();
-ctx.beginPath();
-ctx.arc(40,40,20,pi * 0.75,pi * 1.25,false)
+//setup canvas
+const canvas = document.getElementById('main');
+const cxt = canvas.getContext('2d');
+//variables
+const pi = Math.PI;
+//not variables
+cxt.fillStyle = 'red';
+cxt.fillRect(0,0,40,40);
+cxt.fillStyle='blue';
+cxt.fillRect(1326,728,40,40);
+cxt.beginPath();cxt.moveTo(40,40);
+cxt.lineTo(1326,728);
+cxt.lineWidth = 5;
+function drawPlayer(x,y,size,color) {
+    const halfSize = size/2
+    const centerX = x + halfSize
+    const centerY = y + halfSize
 
+    cxt.fillStyle = color;
+    cxt.fillRect(x,y,size,size);
+    cxt.beginPath()
+    cxt.lineWidth = 5
+    cxt.arc(centerX,centerY,size*0.425,0,pi*2);
+    cxt.fill()
+    cxt.moveTo(centerX,centerY);
+    cxt.lineTo(x,y);
+    cxt.stroke();
+}
+    
+cxt.font = '24px Arial'
+canvas.addEventListener('click',(event)=>{
+    console.log('yaaas queen');
+    const{offsetX: x, offsetY: y} = event;
+    console.log(`Clicked at ${x}, ${y}`);
+});
 
+canvas.addEventListener('mousemove',(event)=>{
+    const{offsetX: x, offsetY: y} = event;
+    cxt.clearRect(0,0,canvas.width,canvas.height);
+    cxt.fillStyle = 'rgba(0,120,255,0.7)';
+    cxt.arc(x, y,50,0,pi*2,true)
+});
